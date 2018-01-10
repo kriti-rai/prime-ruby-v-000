aff08f0987abd9f2c23833b99aa6377ee660bf30
Kriti Rai <kriti.rai@gmail.com>
2018-01-10 20:52:00 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,13 +1,13 @@
 def prime?(num)
-	less_than_num = num-1	
-	array = (2..less_than_num).to_a 
+	less_than_num = num-1
+	array = (2..less_than_num).to_a
 	return true if num==2
 	array.each do |i|
 		if num % i == 0
-			return false 
+			return false
 
 		else
-			return true 
+			return true
 		end
 	end
 end
